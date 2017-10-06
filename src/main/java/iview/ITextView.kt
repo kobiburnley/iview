ad20b52da8865f1d7bed13e6413e62abf542701c
kobi <kobi.b@inner-active.com>
2017-10-07 00:51:34 +0300

    popular

--- src/main/java/iview/ITextView.kt
+++ src/main/java/iview/ITextView.kt
@@ -1,5 +1,21 @@
 package iview
 
-interface ITextView {
+val VISIBLE = 0x00000000
+val INVISIBLE = 0x00000004
+val GONE = 0x00000008
+
+interface IView {
+    fun setVisibility(visibility: Int)
+}
+
+interface ITextView: IView {
     fun setText(text: CharSequence)
+}
+
+interface IImageView: IView {
+
+}
+
+interface IRecycler: IView {
+    var length: Int
 }
