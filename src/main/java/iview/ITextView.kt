e0755defc5ab0b91adfdae46acaed9bbf334817d
kobi <kobi.b@inner-active.com>
2017-10-10 02:32:54 +0300

    top-chart

--- src/main/java/iview/ITextView.kt
+++ src/main/java/iview/ITextView.kt
@@ -1,21 +1,27 @@
 package iview
 
-val VISIBLE = 0x00000000
-val INVISIBLE = 0x00000004
-val GONE = 0x00000008
+object Visibility {
+    val VISIBLE = 0x00000000
+    val INVISIBLE = 0x00000004
+    val GONE = 0x00000008
+}
+
+val Boolean.visibility: Int
+    get() = if (this) Visibility.VISIBLE else Visibility.GONE
+
 
 interface IView {
     fun setVisibility(visibility: Int)
 }
 
-interface ITextView: IView {
+interface ITextView : IView {
     fun setText(text: CharSequence)
 }
 
-interface IImageView: IView {
+interface IImageView : IView {
 
 }
 
-interface IRecycler: IView {
+interface IRecycler : IView {
     var length: Int
 }
